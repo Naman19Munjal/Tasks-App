64d77a76884781ebf0525b9f70aa728193c82bc5
unknown <alexalister1919@gmail.com>
2022-02-03 19:21:20 +0530

    added email functionality

--- src/models/user.js
+++ src/models/user.js
@@ -79,10 +79,7 @@ UserSchema.methods.toJSON = function () {
 
 UserSchema.methods.generateAuthToken = async function () {
   const user = this;
-  const token = jwt.sign(
-    { _id: user._id.toString() },
-    "thisismyfirsttimeusingjwt"
-  );
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
 
   user.tokens = user.tokens.concat({ token });
   await user.save();
